08491ebf8973263add8a571038dfd0296c98a8a8
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 14:15:09 +0530

    Update help.py

--- Amitacloner/plugins/bot/help.py
+++ Amitacloner/plugins/bot/help.py
@@ -1,14 +1,14 @@
 from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message, InlineKeyboardButton
-from Clonify import app
-from Clonify.utils import help_pannel
-from Clonify.utils.database import get_lang
-from Clonify.utils.decorators.language import LanguageStart, languageCB
-from Clonify.utils.inline.help import help_back_markup, private_help_panel
+from Amitacloner import app
+from Amitacloner.utils import help_pannel
+from Amitacloner.utils.database import get_lang
+from Amitacloner.utils.decorators.language import LanguageStart, languageCB
+from Amitacloner.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
-from Clonify.utils.stuffs.helper import Helper
+from Amitacloner.utils.stuffs.helper import Helper
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
