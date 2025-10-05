7dcfe810e12cc10d8d46e2a8fa000de843260f1b
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 23:09:02 +0530

    Update dev.py

--- Amitacloner/plugins/tools/dev.py
+++ Amitacloner/plugins/tools/dev.py
@@ -11,7 +11,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from Clonify import app
+from Amitacloner import app
 from config import OWNER_ID
 ###########
 
