ca223b747386fb8402d3e091f02b19068f0ad806
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 23:03:45 +0530

    Update Gpt.py

--- Amitacloner/plugins/tools/Gpt.py
+++ Amitacloner/plugins/tools/Gpt.py
@@ -1,5 +1,5 @@
 import requests
-from Clonify import app
+from Amitacloner import app
 from pyrogram.types import Message
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
