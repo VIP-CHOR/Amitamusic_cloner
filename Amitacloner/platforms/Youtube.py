a3fe53324e2912c40e15221efc398ad86f93b8e0
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 14:00:11 +0530

    Update Youtube.py

--- Amitacloner/platforms/Youtube.py
+++ Amitacloner/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from Clonify.utils.database import is_on_off
-from Clonify.utils.formatters import time_to_seconds
+from Amitacloner.utils.database import is_on_off
+from Amitacloner.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
