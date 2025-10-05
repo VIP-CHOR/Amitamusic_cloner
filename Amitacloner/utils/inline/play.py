fbf3bd66b816b7d5b3dff5fa5c93965bb9860d41
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 23:22:15 +0530

    Update play.py

--- Amitacloner/utils/inline/play.py
+++ Amitacloner/utils/inline/play.py
@@ -1,9 +1,9 @@
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-from Clonify import app
+from Amitacloner import app
 import config
-from Clonify.utils.formatters import time_to_seconds
+from Amitacloner.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
