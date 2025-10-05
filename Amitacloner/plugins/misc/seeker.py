8d772645f3da58c825bf00509f355e684c6e516a
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 22:41:37 +0530

    Update seeker.py

--- Amitacloner/plugins/misc/seeker.py
+++ Amitacloner/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from Clonify.misc import db
-from Clonify.utils.database import get_active_chats, is_music_playing
+from Amitacloner.misc import db
+from Amitacloner.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
