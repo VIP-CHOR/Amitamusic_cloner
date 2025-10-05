c16305d535e451d2ff8bce22a51524a39a5b0f0f
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 23:35:15 +0530

    Update sys.py

--- Amitacloner/utils/sys.py
+++ Amitacloner/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from Clonify.misc import _boot_
-from Clonify.utils.formatters import get_readable_time
+from Amitacloner.misc import _boot_
+from Amitacloner.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
