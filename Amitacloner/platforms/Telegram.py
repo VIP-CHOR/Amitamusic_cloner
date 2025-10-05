da05f10dd5618c50b3b62c43366d43f986e38305
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 14:01:18 +0530

    Update Telegram.py

--- Amitacloner/platforms/Telegram.py
+++ Amitacloner/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from Clonify import app
-from Clonify.utils.formatters import (
+from Amitacloner import app
+from Amitacloner.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
