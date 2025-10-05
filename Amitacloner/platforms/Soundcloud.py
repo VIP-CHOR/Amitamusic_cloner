ba173408d64208b996f257c8986bc05b91ee7834
VIP-CHOR <vp1576297@gmail.com>
2025-10-05 13:59:03 +0530

    Update Soundcloud.py

--- Amitacloner/platforms/Soundcloud.py
+++ Amitacloner/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from Clonify.utils.formatters import seconds_to_min
+from Amitacloner.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
